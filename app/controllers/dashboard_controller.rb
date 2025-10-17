962bdee3219c5b677944953bb322379e34abde11
vijayda1891 <vijayda2009@gmail.com>
2025-10-17 22:31:08 +0530

    App home page layout and design

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,19 @@ class DashboardController < ApplicationController
 
   before_action :authenticate_user!, only: [:user_dashboard]
 
+  set_tab :user_dashboard
+
   def index
   end
 
   def user_dashboard
+    @main_featured = News.find_by(main_featured: true)
+    @left_sidebar = News.where(main_featured: false).order(created_at: :desc).limit(2)
+    @right_sidebar = News.where(main_featured: false).where.not(id: @left_sidebar.pluck(:id)).order(created_at: :desc).limit(3)
+    exclude_ids = @left_sidebar.pluck(:id) + @right_sidebar.pluck(:id)
+    @more_news = News.where(main_featured: false).where.not(id: exclude_ids).order(created_at: :desc).limit(12)
+    @articles = Article.order(created_at: :desc).limit(4)
+    @game_reviews = GameReview.order(created_at: :desc).limit(4)
+    @book_reviews = BookReview.order(created_at: :desc).limit(4)
   end
 end
